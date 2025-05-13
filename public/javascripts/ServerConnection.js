fec8db1d445f96ae86aa99aad6fc9a020c09220a
Ricky Johnson <ricky.johnson@techsoft3d.com>
2025-05-13 16:00:31 -0400

    update

--- public/javascripts/ServerConnection.js
+++ public/javascripts/ServerConnection.js
@@ -1,41 +1,43 @@
 class ServerConnection {
-    constructor(endpoint) {
-          this._endpoint = endpoint;
-    }
-  
-    connect() {
-          var _this = this;
-          var request = new XMLHttpRequest();
-          request.open("POST", this._endpoint + "/api/spawn");
-          request.overrideMimeType("application/json");
-          request.setRequestHeader("Content-Type", "application/json");
-          request.timeout = 60000;
-  
-          var promise = new Promise(function (resolve, reject) {
-            request.onreadystatechange = function () {
-                  if (request.readyState == 4) {
-                    if (request.status == 200) {
-                          resolve(_this.parseServerSuccessResponse(request.responseText));
-                    }
-                    else {
-                          reject(alert("Couldn't Connect"));
-                    }
-                  }
-            };
-  
-            request.send('{"class":"csr_session","params":{}}');
-          });
-  
-          return promise;
-    }
-  
-    parseServerSuccessResponse(text) {
-      this._jsonResponse = JSON.parse(text);
-      this._endpointuri = this._jsonResponse.endpoints["ws"];
+  constructor(endpoint) {
+    this._endpoint = endpoint;
+  }
 
-      // 🔥 Force WebSocket to use `wss://` instead of `ws://`
-      if (this._endpointuri.startsWith("ws://")) {
-          this._endpointuri = this._endpointuri.replace("ws://", "wss://");
-      }
+  connect() {
+    var _this = this;
+    var request = new XMLHttpRequest();
+    request.open("POST", this._endpoint + "/api/spawn");
+    request.overrideMimeType("application/json");
+    request.setRequestHeader("Content-Type", "application/json");
+    request.timeout = 60000;
+
+    var promise = new Promise(function (resolve, reject) {
+      request.onreadystatechange = function () {
+        if (request.readyState == 4) {
+          if (request.status == 200) {
+            resolve(_this.parseServerSuccessResponse(request.responseText));
+          } else {
+            reject(alert("Couldn't Connect"));
+          }
+        }
+      };
+
+      request.send('{"class":"csr_session","params":{}}');
+    });
+
+    return promise;
   }
+
+  parseServerSuccessResponse(text) {
+    this._jsonResponse = JSON.parse(text);
+    let rawWsUri = this._jsonResponse.endpoints["ws"];
+
+    // Extract the port and session path
+    const url = new URL(rawWsUri);
+    const port = url.port;
+    const sessionPath = url.pathname; // e.g., /session/abc123
+
+    // Rewrite to match Nginx WebSocket proxy
+    this._endpointuri = `wss://${window.location.hostname}/wsproxy/${port}${sessionPath}`;
   }
+}
